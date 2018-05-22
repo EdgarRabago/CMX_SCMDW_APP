2b90461098782ec8a8c281c5c69bd73254535269
Edgar Alan Rabago Salinas <edgar.rabago.salinas@outlook.com>
2018-05-22 14:30:35 -0500

    1st

---
+++ CMX_SCMDW_APP/App_Start/BundleConfig.cs
@@ -0,0 +1,59 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace DataLineageUI
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/minified/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+                      //"~/Content/jsPlumbToolkit-defaults.css",
+                      //"~/Content/jsPlumbToolkit-demo.css",
+                      //"~/Content/app.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/*.css"
+                ));
+
+            var lessBundle = new Bundle("~/Content/less").IncludeDirectory("~/Content", "*.less");
+            lessBundle.Transforms.Add(new LessTransform());
+            lessBundle.Transforms.Add(new CssMinify());
+            bundles.Add(lessBundle);
+
+            //overrides any settings in the web.config file related to bundling and minification
+            //performs minification
+            BundleTable.EnableOptimizations = true;
+
+        }
+    }
+
+    public class LessTransform : IBundleTransform
+    {
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            response.Content = dotless.Core.Less.Parse(response.Content);
+            response.ContentType = "text/css";
+        }
+    }
+}
---
+++ CMX_SCMDW_APP/Attributes/AppAuthorizeAttribute.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CMX_SCMDW_APP.Attributes
+{
+    public class AppAuthorizeAttribute : AuthorizeAttribute
+    {
+     
+
+        
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Controller.ViewBag.ErrorMessage = "User has not been setup in Lineage";            
+            base.HandleUnauthorizedRequest(filterContext);
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            string enableRedirect = WebConfigurationManager.AppSettings["enableRedirect"].ToUpper();
+            string oldURLHostName = WebConfigurationManager.AppSettings["OldUrlHostName"].ToUpper();
+            string newURL = WebConfigurationManager.AppSettings["newUrl"];
+
+            string host = filterContext.HttpContext.Request.Url.Host.ToUpper();
+
+            if (enableRedirect == "TRUE" && host == oldURLHostName)
+            {
+                filterContext.Result = new RedirectResult(newURL, true);
+                return;
+            }
+
+            base.OnAuthorization(filterContext);
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            //PredictivePowerDB db = new PredictivePowerDB("PredictivePower");
+
+            //let the framework log in the user
+            var isAuthorized = base.AuthorizeCore(httpContext);
+
+            //check if user exists in the DB...if not kick them out
+            //var userLoggedIn = db.Users.Where(b => b.UserName == httpContext.User.Identity.Name && b.Status == UserStatus.Active).FirstOrDefault();
+
+            //if (userLoggedIn == null)
+            //{
+            //    isAuthorized = false;
+            //    return isAuthorized;
+            //}
+
+            //var controllerName = (string)httpContext.Request.RequestContext.RouteData.Values["controller"];
+            //var actionName = (string)httpContext.Request.RequestContext.RouteData.Values["action"];
+
+            ////If the routing data contains the customer, then use that customer value and ensure the user has access.
+            //CustomerAccess = (string)httpContext.Request.RequestContext.RouteData.Values["customerName"];
+
+            //if (isAuthorized)
+            //{
+            //    //check if it is a valid customer
+            //    if (CustomerAccess != "")
+            //    {
+            //        //is it a valid customer
+            //        var isCustInList = db.Customers.Any(x => x.CustomerName == CustomerAccess);
+
+            //        if (isCustInList)
+            //        {
+            //            isAuthorized = isValidByCustomer(userLoggedIn) && isValidByGroup(userLoggedIn);
+            //        }
+            //        else
+            //        {
+            //            //user will get kicked out with a 404 on the HasCustomerNameAttribute
+            //            isAuthorized = true;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        isAuthorized = true;
+            //    }                
+            //}
+
+            //db.Dispose();
+
+            return isAuthorized;
+        }  
+
+      
+    }
+
+}
